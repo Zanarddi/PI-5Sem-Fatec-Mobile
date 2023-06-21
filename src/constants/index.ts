ead245fa18c5e2620a847275d031b2528071e321
Zanarddi <gustavo.zanardin@gmail.com>
2023-06-21 15:45:37 -0300

    Adding languages

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,9 @@
 export const LANGUAGES = [
-    { label: "English", code: "en" },
-    { label: "Portuguese", code: "pt" },
-  ];
-  
+  { label: "English", code: "en" },
+  { label: "Portuguese", code: "pt" },
+  { label: "Spanish", code: "es" },
+  { label: "French", code: "fr" },
+  { label: "German", code: "de" },
+  { label: "Japanese", code: "ja" },
+  { label: "Korean", code: "ko" },
+];
--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,6 +2,11 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import EN from './en.json';
 import PT from './pt.json';
+import ES from './es.json';
+import FR from './fr.json';
+import DE from './de.json';
+import JA from './ja.json';
+import KO from './ko.json';
 
 i18n.use(initReactI18next).init({
   lng: i18n.options.lng,
@@ -12,6 +17,11 @@ i18n.use(initReactI18next).init({
   resources: {
     en: { translation: EN },
     pt: { translation: PT },
+    es: { translation: ES },
+    fr: { translation: FR },
+    de: { translation: DE },
+    ja: { translation: JA },
+    ko: { translation: KO },
   },
 });
 
